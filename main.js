700887e9b0f41df2dc72be4a6f4089b01a9452f4
sovinina <kokova03@mail.ru>
2024-03-10 22:46:37 +0300

    f

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    const contents = document.querySelectorAll('li')
     const pageFlip = new St.PageFlip(
         document.getElementById("demoBookExample"),
         {
@@ -18,9 +18,80 @@ document.addEventListener('DOMContentLoaded', function() {
             mobileScrollSupport: false // disable content scrolling on mobile devices
         }
     );
-    document.querySelector('.to').addEventListener('click', function (){
+    /*document.querySelector('.to').addEventListener('click', function (){
         pageFlip.flip(46, 'top')
+    })  */  
+    contents.forEach((btn, index)=>{
+        btn.addEventListener('click', function (){
+            document.querySelector('.toc').classList.add('none')
+            switch (index){
+                case 0:
+                    pageFlip.flip(5, 'top')
+                    break;
+                case 1:
+                    pageFlip.flip(8, 'top')
+                    break;
+                case 2:
+                    pageFlip.flip(10, 'top')
+                    break;
+                case 3:
+                    pageFlip.flip(11, 'top')
+                    break;
+                case 4:
+                    pageFlip.flip(13, 'top')
+                    break;
+                case 5:
+                    pageFlip.flip(17, 'top')
+                    break;
+                case 6:
+                    pageFlip.flip(18, 'top')
+                    break;
+                case 7:
+                    pageFlip.flip(21, 'top')
+                    break;
+                case 8:
+                    pageFlip.flip(22, 'top')
+                    break;
+                case 9:
+                    pageFlip.flip(26, 'top')
+                    break;
+                case 10:
+                    pageFlip.flip(30, 'top')
+                    break;
+                case 11:
+                    pageFlip.flip(31, 'top')
+                    break;
+                case 12:
+                    pageFlip.flip(32, 'top')
+                    break;
+                case 13:
+                    pageFlip.flip(35, 'top')
+                    break;
+                case 14:
+                    pageFlip.flip(38, 'top')
+                    break;
+                case 15:
+                    pageFlip.flip(39, 'top')
+                    break;
+                case 16:
+                    pageFlip.flip(40, 'top')
+                    break;
+                case 17:
+                    pageFlip.flip(41, 'top')
+                    break;
+                case 18:
+                    pageFlip.flip(42, 'top')
+                    break;
+                case 19:
+                    pageFlip.flip(44, 'top')
+                    break;
+            }
+        })
+        
     })
+    /*document.querySelector('.mybtn').addEventListener('click', function (){
+        pageFlip.flip(5, 'top')
+    })*/
     // load pages
     pageFlip.loadFromHTML(document.querySelectorAll(".page"));
 
@@ -80,3 +151,16 @@ for (let i =1;i<47;i++){
     }
 
 }
+
+document.querySelector('.to').querySelector('p').addEventListener('mouseenter',function (){
+    document.querySelector('.toc').classList.remove('none')
+})
+document.querySelector('.to').querySelector('p').addEventListener('mouseleave',function (){
+    document.querySelector('.toc').classList.add('none')
+})
+document.querySelector('.toc').addEventListener('mouseenter',function (){
+    this.classList.remove('none')
+})
+document.querySelector('.toc').addEventListener('mouseleave',function (){
+    this.classList.add('none')
+})
